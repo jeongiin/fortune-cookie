3bd931a17d2b20a1cbf61c8700d73303e201d06b
jeongiin <jeongiin@naver.com>
2023-07-15 21:26:05 +0900

    refactor : 테스트 코드 함수 외부에 존재하도록 변경

--- app/test.py
+++ app/test.py
@@ -1,12 +1,12 @@
 # fortunes.txt 파일이 존재하는 지 테스트
 ## 서비스의 베이스가 되는 데이터이므로 실수로 삭제할 경우 큰 문제가 생김
 import unittest
-import os
+from cookie_maker import load_fortunes_file_path
 
 class TestFortunesFile(unittest.TestCase):
     def test_fortunes_file_exists(self):
-        filename = "fortunes.txt"
-        self.assertTrue(os.path.exists(filename), f"File '{filename}' does not exist.")
+        check_result, _ = load_fortunes_file_path()
+        self.assertTrue(check_result, f"Fortune data does not exist.")
 
 if __name__ == '__main__':
     unittest.main()
