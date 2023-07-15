c07266af01cb30b0f21d0c8f85c955201aaa962d
jeongiin <jeongiin@naver.com>
2023-07-16 01:27:28 +0900

    refactor : 함수로 모듈화

--- app/cookie_maker.py
+++ app/cookie_maker.py
@@ -1,50 +1,38 @@
 import random
 import datetime
-import os
+from utils import *
 
-def load_fortunes_file_path():
-    fortunes_file_folder = os.environ.get('DATAPATH')
-    if fortunes_file_folder is None:
-        fortunes_file_folder = '../data'
-    fortunes_file_path = os.path.join(fortunes_file_folder, "fortunes.txt")
-    
-    if os.path.exists(fortunes_file_path):
-        return (True, fortunes_file_path)
-    
-    return (False, "")
+# 오늘 생성된 문구가 이미 있는지 체크
+def check_existing_fortune(output_file_path):
+    today = datetime.date.today().strftime("%Y-%m-%d")
+    if get_my_fortunes_log_file_path()[0]:
+        with open(output_file_path, "r", encoding="utf-8") as file:
+            for line in file:
+                if today in line:
+                    return True
+    return False
 
-def load_my_fortunes_log_file_path():
-    fortunes_log_file_folder = os.environ.get('LOGPATH')
-    if fortunes_log_file_folder is None:
-        fortunes_log_file_folder = '../data'
-    fortunes_log_file_path = os.path.join(fortunes_log_file_folder, "my_fortunes.txt")
-    
-    if os.path.exists(fortunes_log_file_path):
-        return (True, fortunes_log_file_path)
-    
-    return (False, "")
+# 랜덤으로 한 문구 선택
+def get_random_fortune(fortunes_file_path):
+    with open(fortunes_file_path, "r", encoding="utf-8") as file:
+        fortunes = file.readlines()
+        random_fortune = random.choice(fortunes).strip()
+        return random_fortune
 
 def cookie_maker():
-    # 포춘쿠키 내용이 담긴 텍스트 파일의 경로
-    _, fortunes_file = load_fortunes_file_path()
-
-    # 포춘쿠키 문구를 저장할 파일의 이름과 경로
-    _, log_file = load_my_fortunes_log_file_path()
-
-    # 텍스트 파일에서 포춘쿠키 문구 읽어오기
-    with open(fortunes_file, "r", encoding="utf-8") as file:
-        fortunes = file.readlines()
-
-    # 랜덤으로 한 문구 선택
-    random_fortune = random.choice(fortunes).strip()
-
-    # 현재 시간과 함께 문구를 추가할 파일에 기록
-    current_time = ""
-    with open(log_file, "a", encoding="utf-8") as file:
+    # 오늘 생성된 문구가 없으면 새로운 문구 추가
+    _, fortunes_file_path = get_fortunes_file_path()
+    _, output_file_path = get_my_fortunes_log_file_path()
+    
+    if not check_existing_fortune(output_file_path):
+        random_fortune = get_random_fortune(fortunes_file_path)
         current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-        file.write(f"[{current_time}] {random_fortune}\n")
-
-    print(current_time + "포춘쿠키 문구가 파일에 추가되었습니다.")
+        new_fortune = f"[{current_time}] {random_fortune}\n"
+        with open(output_file_path, "a", encoding="utf-8") as file:
+            file.write(new_fortune)
+            print("새로운 포춘 쿠키 문구가 파일에 추가되었습니다.")
+    else:
+        print("오늘 이미 생성된 포춘 쿠키 문구가 있습니다.")
     
 if __name__ == "__main__":
     cookie_maker()
