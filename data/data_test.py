edbf8f9f4bd0b75331f7379e8f7a1c32c59ede4d
timdalxx <48753785+jeongiin@users.noreply.github.com>
2023-07-16 11:46:57 +0900

    Create data_test.py

---
+++ data/data_test.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+import unittest
+# fortunes.txt 파일이 존재하는 지 테스트
+## 서비스의 베이스가 되는 데이터이므로 실수로 삭제할 경우 큰 문제가 생김
+class TestFortunesFile(unittest.TestCase):
+    def test_fortunes_file_exists(self):
+        check_result = os.path.exists("data/fortunes.txt")
+        self.assertTrue(check_result, f"Fortune data does not exist.")
+
+if __name__ == '__main__':
+    unittest.main()
